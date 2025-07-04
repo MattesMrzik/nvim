022730bca2cdb29aa35d785ae189de908f9c52e1
Mattes Mrzik <mattes@mrzik.de>
2025-07-04 16:23:59 +0200

    cmp accept doesn't overwrite text

--- lua/mattes/rust.lua
+++ lua/mattes/rust.lua
@@ -67,7 +67,7 @@ cmp.setup({
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm(),
+        ["<CR>"] = cmp.mapping.confirm({behaviour = cmp.ConfirmBehavior.Insert}),
         ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
     },
