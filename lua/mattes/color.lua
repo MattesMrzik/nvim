58e4cd1c458c4e1264a547a6cf0c12e3be1c3411
Mattes Mrzik <mattes@mrzik.de>
2025-07-14 16:00:13 +0200

    telescope is transparent

--- lua/mattes/color.lua
+++ lua/mattes/color.lua
@@ -26,12 +26,12 @@ require('kanagawa').setup({
         local theme = colors.theme
         return {
             TelescopeTitle = { fg = theme.ui.special, bold = true },
-            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+            TelescopePromptNormal = { bg = NONE},
+            TelescopePromptBorder = { fg = theme.ui.bg_m1, bg = NONE },
+            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = NONE },
+            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg =NONE },
+            TelescopePreviewNormal = { bg = NONE },
+            TelescopePreviewBorder = { bg = NONE, fg = theme.ui.bg_m1},
             Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },  -- add `blend = vim.o.pumblend` to enable transparency
             PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
             PmenuSbar = { bg = theme.ui.bg_m1 },
