5b1cc0b963890bc4f6bed05b9e9084e493f99a83
Mattes Mrzik <mattes@mrzik.de>
2025-11-27 14:41:42 +0100

    lua auto format

--- lua/mattes/lua_lsp.lua
+++ lua/mattes/lua_lsp.lua
@@ -1,18 +1,27 @@
 vim.lsp.config("lua_ls", {
-  settings = {
-    Lua = {
-      runtime = { version = "LuaJIT" },
-      workspace = {
-        checkThirdParty = false,
-        library = { vim.env.VIMRUNTIME },
-      },
-      diagnostics = {
-        globals = { "vim", "Snacks" },
-      },
+    settings = {
+        Lua = {
+            runtime = { version = "LuaJIT" },
+            workspace = {
+                checkThirdParty = false,
+                library = { vim.env.VIMRUNTIME },
+            },
+            diagnostics = {
+                globals = { "vim", "Snacks" },
+            },
+        },
     },
-  },
 })
 
 -- Enable the server
 vim.lsp.enable("lua_ls")
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.lua",
+    callback = function()
+        vim.lsp.buf.format({
+            async = false,
+            filter = function(client) return client.name == "lua_ls" end,
+        })
+    end,
+})
