ae782270b05bc1478907c7781a9e5b7015da0a04
Mattes Mrzik <mrzi@login02.cm.cluster>
2026-07-22 11:18:47 +0200

    fix path to nextflow lsp

--- lua/mattes/nextflow.lua
+++ lua/mattes/nextflow.lua
@@ -2,7 +2,7 @@ vim.lsp.enable('nextflow')
 
 vim.lsp.config['nextflow'] = {
     -- downloaded from https://github.com/nextflow-io/language-server/releases/tag/v26.04.0
-    cmd = { 'java', '-jar', '/net/home/mrzi/bin/groovy_lspjar' },
+    cmd = { 'java', '-jar', '/net/home/mrzi/bin/groovy_lsp.jar' },
     filetypes = { 'nextflow', 'nf', 'groovy', 'config' },
     root_markers = { 'nextflow.config', '.git' },
     settings = {
